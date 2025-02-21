5acc86823cb3128103923e5e79f8a10dfa6394b8
nasimhossain1 <nasimhossain1@gmail.com>
2025-02-22 02:30:35 +0600

    first step

--- js/login.js
+++ js/login.js
@@ -11,4 +11,19 @@
 
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
+
+    // get phone number and pin
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    //console.log(phoneNumber, pinNumber);
+
+    //bad way to validate
+    if(phoneNumber ==='5' && pinNumber ==='1234'){
+        console.log('you are log in ')
+    }
+    else{
+        alert('wrong phone number and pin')
+    }
+
+
 });
