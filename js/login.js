b255de9d1aeae8ff7f9045362de8829b5445211c
nasimhossain1 <nasimhossain1@gmail.com>
2025-02-22 00:22:53 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// console.log('button add')
+
+// //search: form submit reloading the page
+
+// document.getElementById('button-login').addEventListener('ckick',function(event){
+//     event.preventDefault();
+//     console.log('login button clicked');
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     console.log(phoneNumber)
+// })
+
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
+});
